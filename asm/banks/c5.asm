07121cd8ba53144f795271183142e03688b5dc83
Bropedio <bropedio@gmail.com>
2022-05-01 21:21:06 -0500

    Integrate "Music" hack into banks asm

---
+++ asm/banks/c5.asm
@@ -0,0 +1,11 @@
+hirom
+
+; C5 Bank (music)
+
+; -------------------------------------------------------------------------
+; Expands the total number of songs the game can use and adds a pointer
+; to the new song
+
+org $C53C5E : db $56
+org $C53F95 : dl $D4F806
+
