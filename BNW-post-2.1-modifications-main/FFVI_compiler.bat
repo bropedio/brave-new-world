10048c894855a084da66f7350984c455b83cefe6
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-05-15 21:51:28 +0200

    Update FFVI_compiler.bat
    added references to new asm and ips plus setting flips in tool folder

--- BNW-post-2.1-modifications-main/FFVI_compiler.bat
+++ BNW-post-2.1-modifications-main/FFVI_compiler.bat
@@ -8,6 +8,7 @@ rem ----------------------------------------------------------------------------
 set ASAR="tools\asar.exe"
 set FFVIDECOMP="tools\ffvi.exe"
 set GFX="gfx"
+set flips="tools\flips"
 
 rem ----------------------------------------------------------------------------
 copy %ORIGINAL_ROM% %EDITED_ROM% /y
@@ -18,12 +19,17 @@ echo Applying hacks...
 %ASAR% --pause-mode=on-error %PATCHES%\new_intro.asm %EDITED_ROM%
 %ASAR% --pause-mode=on-error %PATCHES%\misc.asm %EDITED_ROM%
 %ASAR% --pausa-mode=on-error %PATCHES%\veldt-freebies.asm %EDITED_ROM%
+%ASAR% --pausa-mode=on-error %PATCHES%\lore_curative.asm %EDITED_ROM%
+
+echo Applying ips...
+%flips% --apply "graphic_fixes.ips" %EDITED_ROM%
+%flips% --apply "minimap[n].ips" %EDITED_ROM%
 
 echo Compressing GFX
-%FFVIDECOMP% -m c -s 0x02686C %EDITED_ROM% < %GFX%\modified\02686C_Title_Program.bin
-%FFVIDECOMP% -m c -s 0x18F000 %EDITED_ROM% < %GFX%\modified\18F000_Title_GFX.bin
-%FFVIDECOMP% -m c -s 0x19568f %EDITED_ROM% < %GFX%\modified\19568f_Ending_Cinematic_GFX_1.bin
-%FFVIDECOMP% -m c -s 0x199d4b %EDITED_ROM% < %GFX%\modified\199d4b_Ending_Cinematic_GFX_2.bin
-%FFVIDECOMP% -m c -s 0x19a4e5 %EDITED_ROM% < %GFX%\modified\19a4e5_Ending_Cinematic_GFX_3.bin
-%FFVIDECOMP% -m c -s 0x19a800 %EDITED_ROM% < %GFX%\modified\19a800_Map_Tile_Properties.bin
-%FFVIDECOMP% -m c -s 0x19cd10 %EDITED_ROM% < %GFX%\modified\19cd10_Map_Tile_Properties_Pointers.bin
+rem %FFVIDECOMP% -m c -s 0x02686C %EDITED_ROM% < %GFX%\modified\02686C_Title_Program.bin
+rem %FFVIDECOMP% -m c -s 0x18F000 %EDITED_ROM% < %GFX%\modified\18F000_Title_GFX.bin
+rem %FFVIDECOMP% -m c -s 0x19568f %EDITED_ROM% < %GFX%\modified\19568f_Ending_Cinematic_GFX_1.bin
+rem %FFVIDECOMP% -m c -s 0x199d4b %EDITED_ROM% < %GFX%\modified\199d4b_Ending_Cinematic_GFX_2.bin
+rem %FFVIDECOMP% -m c -s 0x19a4e5 %EDITED_ROM% < %GFX%\modified\19a4e5_Ending_Cinematic_GFX_3.bin
+rem %FFVIDECOMP% -m c -s 0x19a800 %EDITED_ROM% < %GFX%\modified\19a800_Map_Tile_Properties.bin
+rem %FFVIDECOMP% -m c -s 0x19cd10 %EDITED_ROM% < %GFX%\modified\19cd10_Map_Tile_Properties_Pointers.bin
