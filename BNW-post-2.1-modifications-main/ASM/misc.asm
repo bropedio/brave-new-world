2cfa345b6b0e2f9edd9d63f25c2f3265d7836485
DESKTOP-68SDJIN\Utenye1 <dodinazareno@gmail.com>
2022-08-15 19:02:20 +0200

    Fixed & arranged a lot of gfx stuff

--- BNW-post-2.1-modifications-main/ASM/misc.asm
+++ BNW-post-2.1-modifications-main/ASM/misc.asm
@@ -3,11 +3,258 @@ hirom
 
 table "menu.tbl", ltr ; Tabella per le stringhe di testo
 
+;;-----------------------------------------------
+;;Changing item list position
+;;-----------------------------------------------
+org $C38D69		;pointers
+	dw #itemresist
+	dw #itemabsorb
+	dw #itemnullify
+	dw #itemweakness
+	
+org $c38df0
+itemresist:
+	db $8d,$7b,"Resist",$00
+
+itemabsorb:
+	db $0d,$7c,"Absorb",$00
+
+itemnullify:
+	db $8d,$7c,"Nullify",$00
+
+itemweakness:
+	db $0d,$7d,"Weakness",$00
+
+
+;;--------------------------------------------------
+;;Item box dimension
+;;	First value: Start print position X assis
+;;      Second value: Start print position Y assis
+;;	Third value: Height
+;;	Fourth value: Lenght
+;;--------------------------------------------------
+
+org $C37E13
+	db $8B,$58,$04,$01	;Item window
+	db $97,$58,$16,$01	;Use_Arrange_Rare window
+	db $4b,$59,$1c,$02	;Description window
+	db $4b,$5a,$1c,$11	;Item List window
+	
+org $C38D10
+	dw #USE
+	dw #ARRANGE
+	dw #RARE
+
+;Data	
+;org $C38D16
+	db $cd,$78,"Item",$00
+	
+USE:
+	db $dd,$78,"USE",$00
+	
+ARRANGE:
+	db $e7,$78,"ARRANGE",$00
+
+RARE:
+	db $f9,$78,"RARE",$00
+	
+;;--------------------------------------------------
+;;Cursor position
+;;	First: Y Assis
+;;	Second value: X Assis
+;;--------------------------------------------------
+
+org $C37D9D
+
+;Use
+dw $0f40
+
+;Arrange
+dw $0f68
+
+;Rare
+dw $0fb0
+
+;Move up description text by 1
+org $c3a73d
+	ldx #$77c9
+
+
+; Load navigation data & Handle D-Pad for Item, Colosseum, or Sell menu
+
+org $C37D1C
+	LDY #Navigation_Data 	; C3/7D2B
+	JMP $05FE				; Load navig data
+	JSR $81C7				; Handle D-Pad
+	LDY #Cursor_Position	; C3/7D30
+	JMP $0648				; Relocate cursor
+	
+padbyte $FF
+pad $c37d43
+
+; Cursor positions for rare item menu
+org $C37D58
+	dw $4a08        ; Item 1
+	dw $4a78        ; Item 2
+	dw $5608        ; Item 3
+	dw $5678        ; Item 4
+	dw $6208        ; Item 5
+	dw $6278        ; Item 6
+	dw $6e08        ; Item 7
+	dw $6e78        ; Item 8
+	dw $7a08        ; Item 9
+	dw $7a78        ; Item 10
+	dw $8608        ; Item 11
+	dw $8678        ; Item 12
+	dw $9208        ; Item 13
+	dw $9278        ; Item 14
+	dw $9e08        ; Item 15
+	dw $9e78        ; Item 16
+	dw $aa08        ; Item 17
+	dw $aa78        ; Item 18
+	dw $b608        ; Item 19
+	dw $b678        ; Item 20
+	
+	
+; HDMA Tables
+; First Value are the scanlines
+
+; BG1 H-Shift table for inventory-style menus
+org $C37EDA
+	db $1C,$00,$01  ; Value to change for moving up the scroll menu
+	db $2F,$00,$01  ; Value to change for moving up the scroll menu
+	db $80,$00,$00  ; Items
+	db $1E,$00,$01  ; Nothing
+	db $00          ; End
+
+; BG1 V-Shift table for inventory-style menus
+;org $C37EE7
+	db $17,$80,$00  ; Value to change for moving up the scroll menu
+	db $08,$00,$00  ; Nothing?
+	db $0C,$04,$00  ; Nothing?
+	db $0C,$08,$00  ; Nothing?
+	db $08,$80,$00  ; Nothing?
+	db $08,$00,$00  ; Nothing?
+	db $04,$bC,$FF  ; Item row 1
+	db $04,$bC,$FF  ; 
+	db $04,$bC,$FF  ; 
+	db $04,$c0,$FF  ; Item row 2
+	db $04,$c0,$FF  ; 
+	db $04,$c0,$FF  ; 
+	db $04,$c4,$FF  ; Item row 3
+	db $04,$c4,$FF  ; 
+	db $04,$c4,$FF  ; 
+	db $04,$c8,$FF  ; Item row 4
+	db $04,$c8,$FF  ; 
+	db $04,$c8,$FF  ; 
+	db $04,$cC,$FF  ; Item row 5
+	db $04,$cC,$FF  ; 
+	db $04,$cC,$FF  ; 
+	db $04,$d0,$FF  ; Item row 6
+	db $04,$d0,$FF  ; 
+	db $04,$d0,$FF  ; 
+	db $04,$d4,$FF  ; Item row 7
+	db $04,$d4,$FF  ; 
+	db $04,$d4,$FF  ; 
+	db $04,$d8,$FF  ; Item row 8
+	db $04,$d8,$FF  ; 
+	db $04,$d8,$FF  ; 
+	db $04,$dC,$FF  ; Item row 9
+	db $04,$dC,$FF  ; 
+	db $04,$dC,$FF  ; 
+	db $04,$e0,$FF  ; Item row 10
+	db $04,$e0,$FF  ; 
+	db $04,$e0,$FF  ; 
+	db $04,$e4,$FF  ; Item row 11
+	db $04,$e4,$FF  ; 
+	db $04,$e4,$FF  ; 
+;Closing table data
+	db $1E,$00,$00  ; Nothing
+	db $00          ; End
+
+
+; Navigation data & Cursor positions for Item, Colosseum, and Sell menus
+
+;C37D2B:
+Navigation_Data:
+	db $01          ; Wraps horizontally
+	db $00          ; Initial column
+	db $00          ; Initial row
+	db $01          ; 1 column
+	db $0B          ; 11 rows
+;C37D30: 
+Cursor_Position: 
+	dw $4a08        ; Item 1
+	dw $5608        ; Item 2
+	dw $6208        ; Item 3
+	dw $6e08        ; Item 4
+	dw $7a08        ; Item 5
+	dw $8608        ; Item 6
+	dw $9208        ; Item 7
+	dw $9e08        ; Item 8
+	dw $aa08        ; Item 9
+	dw $b608        ; Item 10
+	dw $c208		; Item 11
+	
+padbyte $FF
+pad $C37f87
+warnpc $C37f87
+
+org $c30854
+	adc #$0A				; Extend blinking finger to 1 up line	
+
+org $c37e35
+	ldy #Item_Description	; Moving item descritpion HDMA data table in another section
+
+org $c37f8b
+	ldy #$000b				; How many lines must be loaded in the BG1 Tilemap
+
+Org $c37ec6
+	cpx #$007E				; BG1 V-Shift table size
+
+org $c37ed4					; Closing Table data size
+	cpx #$0082
+
+org $C31af3
+	LDA #$0B				; Onscreen rows: 11
+
+org $c31b19
+	lda #$0085				; Extend scroll triangle cursor
+org $c31b20
+	lda #$0046				; Triangle cursor starting point
+
+org $c3265f
+	lda #$0085				; Extend scroll triangle cursor (from USE ARRANGE RARE to scroll)
+org $c32666
+	lda #$0046				; Triangle cursor starting point (from USE ARRANGE RARE to scroll)
+
+org $c3bc0a
+	lda #$0085				; Extend scroll triangle cursor (Shop menu)
+org $c3bc11
+	lda #$0046				; Triangle cursor starting point (Shop menu)
+	
+org $c309b1
+	adc #$0085				; ADD to complete below half cursor scroll
+
+
+
 ;;-----------------------------------------------------
 ;;
 ;;Colosseum
 ;;
 ;;-----------------------------------------------------
+
+; Window layout for Colosseum item menu
+org $C3AD8A
+	dw $588B,$0109  ; 11x04 at $588B (Title)
+	dw $58A1,$0111  ; 19x04 at $58A1 ("Select...")
+	dw $594B,$021C  ; 30x05 at $598B (Description)
+	dw $5A4B,$111C  ; 30x17 at $5ACB (Items)
+
+; Positioned text for Colosseum
+org $C3AD9A
+	dw $78cd : db "Colosseum",$00
+	dw $78e3 : db "Select an Item",$00
 
 ;Change words position
 org $C3B251
--- BNW-post-2.1-modifications-main/ASM/shop.asm
+++ BNW-post-2.1-modifications-main/ASM/shop.asm
@@ -3,11 +3,178 @@ hirom
 
 table "menu.tbl", ltr ; Tabella per le stringhe di testo
 
+; Window layout for Sell menu and main shop menu
+org $C3BC1B
+	dw $588B,$0107  ; 09x04 at $588B (Title)
+	dw $589D,$0113  ; 21x04 at $589D (Dialogue)
+	dw $594B,$0210  ; 18x04 at $598B (Options)
+	dw $596F,$020A  ; 12x04 at $59AF (GP)
+	dw $5A4B,$111C  ; 30x18 at $5A8B (Items)
+
+; Window layout for Buy menu
+	dw $608B,$0107  ; 09x04 at $608B (Title)
+	dw $609D,$0113  ; 21x04 at $609D (Dialogue)
+	dw $614B,$0d1c  ; 30x14 at $618B (Items)
+	dw $650B,$061c  ; 30x08 at $650B (Actors)
+
+; Window layout for shop order menus
+	dw $688B,$0107  ; 09x04 at $688B (Title)
+	dw $689D,$0113  ; 21x04 at $689D (Dialogue)
+	dw $694B,$0711  ; 19x09 at $698B (Item)
+	dw $6B8B,$041C  ; 30x05 at $6BCB (Description)
+	dw $6D0B,$061C  ; 30x08 at $6D0B (Actors)
+	dw $6971,$0709  ; 11x09 at $69B1 (Amounts)
+
+; Window layout for shop "Hold Y" menu
+org $C3fbb4
+	dw $714b,$071c	; Title
+	dw $750b,$061c	; Values
+	dw $708b,$011c	; Description
+	dw $738b,$041c	; Actors
+
+; BG3 text shifting table for shop
+Org $C3C037
+	db $1F,$08,$00  ; Title
+	db $08,$08,$00  ; Item 1
+	db $0C,$0C,$00  ; Item 2
+	db $0C,$10,$00  ; Item 3
+	db $0C,$14,$00  ; Item 4
+	db $0C,$18,$00  ; Item 5
+	db $0C,$1C,$00  ; Item 6
+	db $0C,$20,$00  ; Item 7
+	db $0C,$24,$00  ; Item 8
+	db $0C,$28,$00  ; Nothing
+	db $0C,$2C,$00  ; Nothing
+	db $0C,$30,$00  ; Nothing
+	db $0C,$34,$00  ; Nothing
+	db $0C,$38,$00  ; Nothing
+	db $0C,$3C,$00  ; Nothing
+	db $0C,$40,$00  ; Nothing
+	db $00          ; End
+	
+;Buy_Sell_Exit Cursor value	
+org $C3B89a
+	dw $2a08	;Buy
+	dw $2a30	;Sell
+	dw $2a60	;Exit	
+
+;Weapon cursor value
+org $C3b8b4
+	dw $2a00
+	dw $3600
+	dw $4200
+	dw $4e00
+	dw $5a00
+	dw $6600
+	dw $7200
+	dw $7e00
+
+;Rows in sell menu
+org $C3BBe9
+	LDA #$0B	;11 rows
+	
+
+org $C3b858
+	LDX #$0029	; Cursor in selling item sub menu
+org $c3b65a
+	LDX #$0029	; Cursor in selling item sub menu
+
 ;shop fixes to tools and scrolls value
 
-;new data in new bank
+;rearrange new shop data and gain enough space for fix ninja scroll data and
+;add HDMA scroll item menu data
 
-org $C3FE30
+;Pointers
+
+org $C3B98A
+	dw #holdy
+	
+org $c3f885
+	dw #shopynamebox
+org	$C3F88B
+	dw #shopystatbox
+org	$C3F891
+	dw #shopydescritionbox
+org	$C3F897
+	dw #shopycharabox	
+	
+org $C3f905
+
+	LDY #shopvigor				;pointer
+	JSR $02f9					;Subroutine that print letter
+	LDY #shopspeed			
+	JSR $02f9
+	LDY #shopstamina		
+	JSR $02f9
+	LDY #shopmagic
+	JSR $02f9
+	LDY #shopstatdefense
+	JSR $02f9
+	LDY #shopmdef
+	JSR $02f9
+	LDY #shopevade			
+	JSR $02f9
+	LDY #shopmevade			
+	JSR $02f9
+	LDY #shopattack
+	JSR $02f9
+
+
+org $C3F9E4
+	dw #shopatthyphens
+org	$C3FA10
+	dw #shopdefhyphens		;Defense hyphens pointer
+org $C3FA16
+	dw #shopmdefhyphens		;Magic Defense hyphens pointer
+
+org $C3FA1E
+	dw #shopquestionmarks
+
+;Text data
+org $C3FB0A
+
+holdy:
+	db $1F,$79,"Y",$fe,"per",$fe,"dettagli",$c5,$00
+shopvigor:
+	db $0d,$82,"Vigor",$00
+shopspeed:
+	db $0d,$83,"Speed",$00
+shopstamina:
+	db $8d,$83,"Stamina",$00
+shopmagic:
+	db $8d,$82,"Magic",$00
+shopstatdefense:
+	db $2b,$82,"Defense",$00
+shopmdef:
+	db $2b,$83,"M.Def.",$00
+shopevade:
+	db $ab,$82,"Evade",$00
+shopmevade:
+	db $ab,$83,"M.Evade",$00
+shopattack:
+	db $2b,$81,"Attack",$00
+shopquestionmarks:
+	db $3f,$81,$bf,$bf,$bf,$00	 		;Question marks 
+shopatthyphens:
+	db $3f,$81,"---",$00				;Attack hyphens
+shopdefhyphens:
+	db $3f,$82,"---",$00				;Defense hyphens
+shopmdefhyphens:
+	db $3f,$83,"---",$00				;M.Defense hyphens
+shopynamebox: 
+	db $4b,$71,$1c,$07				;BCG
+shopystatbox:
+	db $0b,$75,$1c,$06				;BCG
+shopydescritionbox:
+	db $8b,$70,$1c,$01				;BCG
+shopycharabox:
+	db $8b,$73,$1c,$04				;BCG	
+	
+padbyte $ff
+pad $C3fbbe
+warnpc $c3fbbf
+
+org $c3fbbf
 shop0attack:
 	db $3f,$81,"  0",$00		;0 set instead of - in the item shop
 shop0defense:
@@ -16,7 +183,43 @@ shop0mdefense:
 	db $3f,$83,"  0",$00		;0 set instead of - in the item shop
 shop100attack:
 	db $3f,$81,"100",$00		;100 set in the item shop
+
+set_100_scroll:
+	ldy #shop100attack				;Load "100" attack value in Shop Menu
+	cpx #$1428						;Are you on Ninja scroll water?
+	beq	13							;Branch to 100 if is it
+	cpx #$140a              		;Are you on Ninja scroll fire?
+	beq	8                   		;Branch to 100 if is it       
+	cpx #$1446						;Are you on Ninja scroll bolt?
+	beq	3							;Branch to 100 if is it
+	ldy	#shop0attack				;Load "0" attack value in Shop Menu
+	jmp $fa9d						;Go back to original routine and start print
 	
+; BG3 V-Shift table for Item and Colosseum menus
+; original data on c37f57 adress
+
+Item_Description:
+	db $2F,$00,$00  ; Title
+	db $0C,$04,$00  ; Desc row 1
+	db $0C,$08,$00  ; Desc row 2
+	db $0C,$0C,$00  ; 2-hand
+	db $0C,$10,$00  ; 50% Dmg
+	db $0C,$14,$00  ; Vigor
+	db $0C,$18,$00  ; Speed
+	db $0C,$1C,$00  ; Stamina
+	db $0C,$20,$00  ; Mag.Pwr
+	db $0C,$24,$00  ; Bat.Pwr
+	db $0C,$28,$00  ; Defense
+	db $0C,$2C,$00  ; Evade
+	db $0C,$30,$00  ; Mag.Def
+	db $0C,$34,$00  ; MBlock
+	db $0C,$38,$00  ; Nothing
+	db $0C,$3C,$00  ; Nothing
+	db $00          ; End
+
+warnpc $c3fc20
+
+
 ;Stats value
 
 org $C3F972
@@ -87,18 +290,6 @@ avoid_btl_equip_routine:
 
 org $c3fa9a	
 	jmp set_100_scroll				;Jump to new code instead of Shop 0 Attack value
-
-org $c3fe50
-set_100_scroll:
-	ldy #shop100attack				;Load "100" attack value in Shop Menu
-	cpx #$1428						;Are you on Ninja scroll water?
-	beq	13							;Branch to 100 if is it
-	cpx #$140a              		;Are you on Ninja scroll fire?
-	beq	8                   		;Branch to 100 if is it       
-	cpx #$1446						;Are you on Ninja scroll bolt?
-	beq	3							;Branch to 100 if is it
-	ldy	#shop0attack				;Load "0" attack value in Shop Menu
-	jmp $fa9d						;Go back to original routine and start print
 
 
 ;;-----------------------------------------------------
