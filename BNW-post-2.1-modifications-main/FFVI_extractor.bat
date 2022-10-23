2c691318e32f5b93aff17822b80906eb599dfb75
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-10-23 11:33:31 +0200

    Update FFVI_extractor.bat
    new extractor meant to extract font and dialogues directly from a base headered vanilla rom edited via ff3usme.exe

--- BNW-post-2.1-modifications-main/FFVI_extractor.bat
+++ BNW-post-2.1-modifications-main/FFVI_extractor.bat
@@ -1,36 +1,22 @@
 @echo off
 rem ----------------------------------------------------------------------------
-set ROM="rom\bnw.sfc"
+set ROM="scripts\Bnw_2.1.smc"
 rem ----------------------------------------------------------------------------
+set SCRIPTS="scripts"
 set GFX="gfx"
 rem ----------------------------------------------------------------------------
 set DD="tools\dd.exe"
-set FFVIDECOMP="tools\ffvi.exe"
 
 rem ----------------------------------------------------------------------------
 
 rem ----------------------------------------------------------------------------
-if not exist "%GFX%" mkdir %GFX%
-if not exist "%GFX%\de-compressed" mkdir "%GFX%\de-compressed"
-if not exist "%GFX%\modified" mkdir ""%GFX%\modified"
-if not exist "%GFX%\extracted" mkdir "%GFX%\extracted"
+if not exist "%SCRIPTS%" mkdir %SCRIPTS%
 rem ----------------------------------------------------------------------------
 
 rem ----------------------------------------------------------------------------
-echo Extracting Uncompressed Graphics...
-%DD% skip=57632 count=512 if=%ROM% of=%GFX%\extracted\00E120_battle_data.bin bs=1 2>NUL
-%DD% skip=294848 count=9472 if=%ROM% of=%GFX%\extracted\047FC0_menu_font.bin bs=1 2>NUL
-%DD% skip=2223552 count=1024 if=%ROM% of=%GFX%\extracted\21EDC0_INN.bin bs=1 2>NUL
-%DD% skip=2712320 count=1536 if=%ROM% of=%GFX%\extracted\296300_The_End.bin bs=1 2>NUL
-
-rem ----------------------------------------------------------------------------
-echo Extracting Compressed Graphics...
-%FFVIDECOMP% -m d -s 0x02686C %ROM% > %GFX%\de-compressed\02686C_Title_Program.bin
-%FFVIDECOMP% -m d -s 0x04BA00 %ROM% > %GFX%\de-compressed\04BA00_End_Font_GFX.bin
-%FFVIDECOMP% -m d -s 0x12E000 %ROM% > %GFX%\de-compressed\12E000_Battle_GFX.bin
-%FFVIDECOMP% -m d -s 0x18F000 %ROM% > %GFX%\de-compressed\18F000_Title_GFX.bin
-%FFVIDECOMP% -m d -s 0x19568f %ROM% > %GFX%\de-compressed\19568f_Ending_Cinematic_GFX_1.bin
-%FFVIDECOMP% -m d -s 0x199d4b %ROM% > %GFX%\de-compressed\199d4b_Ending_Cinematic_GFX_2.bin
-%FFVIDECOMP% -m d -s 0x19a4e5 %ROM% > %GFX%\de-compressed\19a4e5_Ending_Cinematic_GFX_3.bin
-%FFVIDECOMP% -m d -s 0x19a800 %ROM% > %GFX%\de-compressed\19a800_Map_Tile_Properties.bin
-%FFVIDECOMP% -m d -s 0x19cd10 %ROM% > %GFX%\de-compressed\19cd10_Map_Tile_Properties_Pointers.bin
+echo Extracting Scripts...
+%DD% skip=295360 count=9472 if=%ROM% of=%GFX%\047FC0_menu_font.bin bs=1 2>NUL
+%DD% skip=57760 count=256 if=%ROM% of=%SCRIPTS%\DTE_table.bin bs=1 2>NUL
+%DD% skip=845824 count=133887 if=%ROM% of=%SCRIPTS%\town_dialog.bin bs=1 2>NUL
+%DD% skip=1040864 count=5231 if=%ROM% of=%SCRIPTS%\short_battle_dialog.bin bs=1 2>NUL
+%DD% skip=1102336 count=11519 if=%ROM% of=%SCRIPTS%\long_battle_dialog.bin bs=1 2>NUL
