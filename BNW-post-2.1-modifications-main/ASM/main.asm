06f1d9c49339615bfcf4da5237e42e68e9ce8646
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-10-23 12:04:04 +0200

    Update main.asm

--- BNW-post-2.1-modifications-main/ASM/main.asm
+++ BNW-post-2.1-modifications-main/ASM/main.asm
@@ -4,24 +4,22 @@
 ;; Region: USA [!]
 ;;------------------------------------------------------------------------------
 
-;uncomment to activate patches ;activate just one speed patch
+;comment patches to deactivate them
 
-incsrc "c3bis.asm"
+incsrc "shrinked_colosseum.asm"
 incsrc "new_intro.asm"
-incsrc "misc.asm"
 incsrc "veldt-freebies.asm"
 incsrc "lore_curative.asm"
 incsrc "shop.asm"
 incsrc "longer_skills.asm"
-incsrc "mp_colors.asm"
+incsrc "misc.asm"
 incsrc "encounter.asm"
 incsrc "status_menu.asm"
 incsrc "item.asm"
-;incsrc "levelcap.asm"
-;incsrc "inventory_limits.asm"
-;incsrc "coreography.asm"
-;incsrc "speedy-enemies.asm"
-;incsrc "slow-players.asm"
-;incsrc "speedy-enemies-slow-players.asm"
-;incsrc "relm_dog.asm"
-;incsrc "control_colosseum.asm"
+incsrc "scripts.asm"
+incsrc "wallchange.asm"
+incsrc "descriptions.asm"
+incsrc "party.asm"
+incsrc "halfturn_blitz.asm"
+incsrc "better_flags.asm"
+;incsrc "mp_colors.asm"
