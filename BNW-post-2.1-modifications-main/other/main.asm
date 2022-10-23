a55fdffac6d77dff96f2cbf7447c916262c583af
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-10-23 11:28:03 +0200

    folder with optional asm
    optional asm meant to be compiled on a patched rom using FFVI_compiler2.bat in the main folder

---
+++ BNW-post-2.1-modifications-main/other/main.asm
@@ -0,0 +1,14 @@
+;;------------------------------------------------------------------------------
+;; Game: FFVI Brave New World
+;; Platform: SNES
+;; Region: USA [!]
+;;------------------------------------------------------------------------------
+
+;uncomment to activate patches ;activate just one speed patch
+
+;incsrc "levelcap.asm"
+;incsrc "inventory_limits.asm"
+;incsrc "coreography.asm"
+;incsrc "speedy-enemies.asm"
+;incsrc "slow-players.asm"
+;incsrc "speedy-enemies-slow-players.asm"
