55375946f7b8b8efa8d8da4b855558cfd4ebbbd8
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-05-24 16:42:46 +0200

    Update names.asm

--- BNW-post-2.1-modifications-main/vnw/names.asm
+++ BNW-post-2.1-modifications-main/vnw/names.asm
@@ -7,22 +7,31 @@ table "menu.tbl",ltr
 ;Vanilla New World names
 ;-------------------------
 
+org $D2BE60
+	db $E7,"Peace>Ring  "	; Codpiece
+
+org $D2BEAE
+	db $E7,"Charm>Bangle"	; Dirty Undies
+
 org $D2BFBF
 
-    db $EB,"Chocobo>Wing"
-    db $EB,"Hero>Drink  "
+    db $EB,"Chocobo>Wing"	; Red Bull
+    db $EB,"Hero>Drink  "	; Slim Jim
 
 org $CEFC08
 
-    db "Coral        "
-    db "Books        "
+    db "Coral        "		; Booty
+    db "Books        "		; Filth
 
 org $CFD3C8
 
-    db "Mind>Sting"
+    db "Mind>Sting"			; 8=====>~
 	
 org $CFDC88
 
+    db "Ink       "			; Squirt
     db "Ink       "
     db "Ink       "
-    db "Ink       "
+	
+org $E6FD23
+	db "Demon>Eye  "		; Hurricane
