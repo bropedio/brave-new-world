7cb5ab6b5e269465d8c5eb3696b48c23ef6b1e12
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-06-08 13:59:41 +0200

    Gogo tutorial and code for sealed door sound

---
+++ BNW-post-2.1-modifications-main/ASM/events.asm
@@ -0,0 +1,58 @@
+arch 65816
+hirom
+  
+; Moving Mog tutorial from South Figaro (relics) to Gogo's room (commands customization)
+
+org $CA790E
+  db $FD,$FD,$FD,$FD                        ; Deactivate the cutscene 
+
+org $CB52F3
+  db $B2,$C1,$C5,$00,$B2,$F6,$D2,$02,$FE	; New subroutine
+  
+org $CB823F 
+  db $B2,$F3,$52,$01            			; Call new subroutine
+
+org $CCD31D
+  db $4B,$40,$0A                			; Display [caption #2623]:
+                                            ; <A12> can do much more than just [Mimic].<D>
+                                            ; Customize their command list on the status page.
+org $CCD321
+  db $6A,$16,$21,$08                  		; Change Map: Gogo's room at (8,6)
+
+
+;;; Sealed by... song data (overwritten song ends at $C98D84, added twelve 00 bytes)	
+;;org $C98CE8
+;;incbin sealed_song.bin
+;;
+;;; Sealed by... instruments data ($C54695 is the offset for a perfect replica of the same instruments)
+;;org $C548B5
+;;incbin sealed_inst.bin
+;;
+;;; replace "machine running" with "wind" song during cranes event
+;;org $CC82C9
+;;  db $39
+;
+;; Change the event tile at the exit to Atma's room
+;
+;org $C414E2
+;  db $FA
+;
+;; Blocks an exit so Atma is a required boss
+;; Atma sequence starts at $CC18B4
+;
+;org $CC18BE : db $B2,$FD,$52,$01   ; JSR $CB52FD
+;
+;; Helper for making Atma required boss in KT
+;
+;org $CB52FD
+;  db $42,$10,$DD,$BD         ; Displaced code from originating location
+;  db $D0,$E1                 ; Set event bit $1E80($0E1) [1E9C, bit 1]
+;  db $FE                     ; RTS
+;; $CB5304
+;  db $C0,$E1,$80,$5C,$13,$02 ; If ($1E80($0E1) [$1E9C, bit 1] is set), branch to $CC135C
+;  db $31,$82                 ; Open action queue for on-screen character
+;  db $80,$FF                 ; Move character up 1 tile, end queue
+;  db $F0,$38                 ; Play "Nighty Night"
+;  db $4B,$FB,$00             ; Caption 250
+;  db $F3,$10                 ; Fade in previously faded out song with trans. time 16
+;  db $FE                     ; RTS
