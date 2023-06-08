ee6c57f50a38584f691a7c0df583339ab0e0d8db
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-06-08 14:20:59 +0200

    Update FFVI_extractor.bat

--- BNW-post-2.1-modifications-main/vnw/FFVI_extractor.bat
+++ BNW-post-2.1-modifications-main/vnw/FFVI_extractor.bat
@@ -1,4 +1,6 @@
 echo Extracting Scripts...
+dd skip=57760 count=256 if=vnw.smc of=DTE_table.bin bs=1 2>NUL
 dd skip=845824 count=133887 if=vnw.smc of=town_dialog.bin bs=1 2>NUL
 dd skip=1040864 count=5231 if=vnw.smc of=short_battle_dialog.bin bs=1 2>NUL
 dd skip=1102336 count=11519 if=vnw.smc of=long_battle_dialog.bin bs=1 2>NUL
+
