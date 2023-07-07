b99ccf4ce127d3b287a7562fc778db7d9cc8368a
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-07-07 13:46:09 +0200

    Add files via upload

---
+++ BNW-post-2.1-modifications-main/ASM/reserved_freespace.asm
@@ -0,0 +1,18 @@
+hirom
+
+; This assembly file "reserves" freespace for important alt patches by padding
+; them with an unconventional padding byte
+
+padbyte $EE
+
+; notext_noswitch
+
+org $C0FF18 : pad $C0FF8E ; 118 bytes
+
+; choreography
+
+org $C23AC5 : pad $C23AEE ; 41 bytes
+
+; new_game_plus (Bropedio's randomizer)
+
+org $C3F612 : pad $C3F646 ; 52 bytes
