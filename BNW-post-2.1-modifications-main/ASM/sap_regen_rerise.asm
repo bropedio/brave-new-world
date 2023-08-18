fc984bc3e0098c9b01ed5f5b39c4f0efdccd48f3
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-08-18 14:46:03 +0200

    Update sap_regen_rerise.asm

--- BNW-post-2.1-modifications-main/ASM/sap_regen_rerise.asm
+++ BNW-post-2.1-modifications-main/ASM/sap_regen_rerise.asm
@@ -7,9 +7,9 @@ hirom
 	
 org $C2ADE1
 	db $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff	;overwrite KO
-	db $12,$13,$14,$15,$ff,$ff,$ff,$ff,$ff,$ff	;Regen
-	db $12,$18,$19,$1a,$ff,$ff,$ff,$ff,$ff,$ff	;Rerise
-	db $12,$13,$14,$15,$12,$18,$19,$1a,$ff,$ff	;Regen Rerise
-	db $16,$17,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff	;Sap
+	db $ff,$20,$21,$22,$ff,$ff,$ff,$ff,$ff,$ff	;Regen
+	db $ff,$20,$23,$24,$ff,$ff,$ff,$ff,$ff,$ff	;Rerise
+	db $ff,$20,$21,$22,$ff,$20,$23,$24,$ff,$ff	;Regen Rerise
+	db $ff,$28,$29,$ff,$ff,$ff,$ff,$ff,$ff,$ff	;Sap
 	db $82,$87,$84,$80,$93,$84,$91,$ff,$ff,$ff
-	db $16,$17,$ff,$12,$18,$19,$1a,$ff,$ff,$ff	;Sap Rerise
+	db $ff,$28,$29,$ff,$ff,$20,$23,$24,$ff,$ff	;Sap Rerise
