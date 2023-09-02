bcf55c8a7542768ecec81f778766304a9cab885b
Bropedio <bropedio@gmail.com>
2023-09-01 19:36:26 -0500

    Integrate "Randomize Final Party" hack into banks asm

--- asm/banks/c3.asm
+++ asm/banks/c3.asm
@@ -2238,6 +2238,33 @@ DescTilemap:
   JMP $A783       ; Do line 2, row 2
 
 ; #########################################################################
+; Sustain Final Battle Lineup Menu (74)
+;
+; Modify existing line-up code to allow only 4 selections,
+; and add a hook to shuffle the remaining characters when
+; the player presses "Start" or selects "End"
+
+org $C3AA8F
+  JSR MaybeAdd        ; add chosen character if not at max
+  LDA $4A             ; (vanilla code)
+  CMP #$04            ; go to "End" after selecting 4
+
+org $C3AA9C
+  JSR ShuffleList     ; randomize remaining lineup order
+
+; #########################################################################
+; Draw Final Battle Lineup Menu
+
+org $C3AC33
+  LDX #$0004          ; limit user selected list to 4
+
+; -------------------------------------------------------------------------
+; Change lineup menu heading
+
+org $C3AC98
+  dw $391D : db "Select party",$00
+
+; #########################################################################
 ; Sustain Main Shop Menu
 
 ; Modifies "buy item" list handling to update full details ("Shop Hack")
@@ -3087,6 +3114,65 @@ MeleeParamsLong:
 .exit
   CLC
   JML RTS_C2
+
+; -------------------------------------------------------------------------
+; Helpers for Randomize Final Party patch
+;
+; For the final battle against Kefka, only allow the
+; player to select their first 4 characters in the
+; line-up. After those four, the line-up will now be
+; shuffled/randomized for an extra challenge.
+
+org $C3F5C0
+
+MaybeAdd:
+  LDA $4A             ; currently selected
+  CMP #$04            ; already maxed out
+  BEQ .exit           ; exit (and point to End) if ^
+  JSR $AAB2           ; add chosen character (vanilla)
+.exit
+  RTS
+
+ShuffleList:
+  LDY #$000B          ; start with 12th slot
+.loop
+  TYA                 ; place range in A
+  JSR RandomInRange   ; random number from 0 to A
+  TAX                 ; store as slot to swap
+  JSR SwapSlots       ; swap slots X and Y
+  DEY                 ; point to next lowest slot
+  BNE .loop           ; peform random swaps for each slot
+  JSR $AACA           ; continue to "End" vanilla code
+  RTS
+
+RandomInRange:        ; 18 bytes
+  INC                 ; include A in possible random number
+  STA $4202           ; first multiplicand (range)
+  JSL $C0FD00         ; random number
+  STA $4203           ; second multiplicand
+  NOP #4              ; wait for calc
+  LDA $4217           ; get hi byte of product
+  RTS
+
+SwapSlots:            ; 35 bytes
+  PHB                 ; store current bank
+  LDA #$7E            ; replace with $7E
+  PHA
+  PLB
+  LDA $AA8D,X         ; slot X's palette
+  PHA                 ; store on stack
+  LDA $AA8D,Y         ; slot Y's palette
+  STA $AA8D,X         ; replace X's
+  PLA                 ; get X's palette
+  STA $AA8D,Y         ; replace Y's
+  LDA $9D8A,X         ; slot X's id
+  PHA                 ; store on stack
+  LDA $9D8A,Y         ; slot Y's id
+  STA $9D8A,X         ; replace X's
+  PLA                 ; get X's id
+  STA $9D8A,Y         ; replace Y's
+  PLB                 ; restore bank
+  RTS
 
 ; -------------------------------------------------------------------------
 org $C3F700
--- asm/bropedio-2.0/RC33.asm
+++ asm/bropedio-2.0/RC33.asm
@@ -7,7 +7,6 @@ hirom
 ; This master file includes all bugfixes and patches for RC33
 ; of Brave New World 2.0.
 
-incsrc random-party.asm
 incsrc palidor-redux.asm
 incsrc stray-fix.asm
 incsrc mug-better.asm               ; requires "inform-miss-3", "xkill-anim"
