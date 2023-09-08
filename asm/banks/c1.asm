99ccf4c802a4ae67b373e52f71e294a6322f235e
Bropedio <bropedio@gmail.com>
2023-09-07 21:59:24 -0500

    Integrate "Item Overflow" hack into banks asm

--- asm/banks/c1.asm
+++ asm/banks/c1.asm
@@ -11,6 +11,15 @@ hirom
 org $C10CFA : JSR CheckSel
 
 ; ########################################################################
+; New Items Buffer
+;
+; Fix Item Buffer Overflow
+; Was causing some items to get eaten up when Weapon Swapping too
+; many times in a row.
+
+org $C112D5 : CPX #$0050 ; wipe entire "pending item transfer" buffer
+
+; ########################################################################
 ; RNG
 
 org $C11861 : JSL Random
