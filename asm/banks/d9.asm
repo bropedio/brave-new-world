afd25a72790170c7ee6f84c9ec7f5dd279f4b567
Bropedio <bropedio@gmail.com>
2026-09-10 13:42:29 -0500

    Integrate `more-walkable-beach.asm`

--- asm/banks/d9.asm
+++ asm/banks/d9.asm
@@ -1,16 +1,14 @@
 hirom
 
-; Patch: More Walkable Beach
-; Author: Fëanor
-; From: more-walkable-beach.ips
-;
-; $D9CA1A:$D9CC4E - Map Tile Properties (compressed)
-; $D9CD5E:$D9CD63 - Pointers to Map Tile Properties
-;
-; This patch makes walkable more beach tiles in solitary island so
-; that grabbing fishes for Cid is now much easier and fast
+; Bank $D9
 
-; These changes are all in compressed code area (I think)
+; ###########################################################################
+; Map Tile Properties (compressed) [?]
+; TODO: Maybe move this data into asm/compressed
+
+; ---------------------------------------------------------------------------
+; Modified by `more-walkable-beach.asm`
+
 org $D9CA1A : db $be
 org $D9CA27 : db $e9,$27,$14,$e7,$0f,$04,$00,$02,$f9,$07,$02,$fd
 org $D9CA39 : db $12,$00,$fc,$2f,$1d,$08,$1a,$30,$32,$28,$2c,$60,$3a,$10,$3c
@@ -20,5 +18,5 @@ org $D9CA64 : db $a5,$00,$93,$08,$b7,$10,$40,$21,$20,$8d,$10,$81,$20,$7e,$08,$93
 org $D9CA7E : db $7b,$18,$f0,$08,$e2,$30,$04,$01,$8f,$f9,$00,$31,$8f,$fd
 org $D9CA93 : incbin bin/more-walkable-beach.bin : warnpc $D9CC4E
 
-; This code is *not* compressed
+; This pointer code is maybe not compressed
 org $D9CD5E : dw $22D8,$233C,$23BA ; update map tile property pointers 
